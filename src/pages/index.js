38f7fb0a0b8c9cbb873e7704ecead1c022cf8038
jude Aloye <judeoaloye@gmail.com>
2024-10-30 16:48:25 +0100

    jude

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,6 +30,7 @@ export default function Home() {
           <li className="mb-2">
             This is Matthew Branch, Do not touch
           </li>
+          <li className="mb-2">Simeon the drug lord</li>
         </ol>
       </main>
     </div>
