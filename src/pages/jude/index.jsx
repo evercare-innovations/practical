3d379869ae4b3b2e7a62ec2ebe457067af1a45be
jude Aloye <judeoaloye@gmail.com>
2024-11-05 16:55:51 +0100

    my new push

--- src/pages/jude/index.jsx
+++ src/pages/jude/index.jsx
@@ -17,7 +17,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            This is Aloye Branch, Do not Run. You are in a land mine
+            This is Aloye Branch, Do not Run.
           </li>
         </ol>
       </main>
