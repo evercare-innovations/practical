72825d357920226a55f0affb63b1e7572feaa869
TOHEEB ORIRE YINUSA <YINUSATOHEEBO@GMAIL.COM>
2024-11-05 17:00:53 +0100

    check

--- src/pages/orire/index.jsx
+++ src/pages/orire/index.jsx
@@ -17,7 +17,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            This is fortune Branch, Do not touch
+            This is fortune Branch, do not touch....
           </li>
         </ol>
       </main>
