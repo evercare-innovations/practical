b502db394d5d5e970e8c4c7dad410006ac7e178e
Aloba Matthew <martmaldigital@gmail.com>
2024-11-05 16:29:20 +0100

    feat: initiate project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import localFont from "next/font/local";
 
 const geistSans = localFont({
@@ -18,17 +17,9 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-4xl text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            This is Matthew Branch, Do not touch
+            This is Home
           </li>
         </ol>
       </main>
